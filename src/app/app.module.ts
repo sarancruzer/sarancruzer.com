c3b0c41d1f8ee9800515c8886bbca9d92427c434
Saravanan Nandhan <saravanan.nandhan@rntbci.com>
2020-02-26 21:11:13 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { WorksComponent } from './components/works/works.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ResumeComponent,
+    WorksComponent,
+    BlogsComponent,
+    ContactComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
